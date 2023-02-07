cd19615c410a9aef189e072335626b8f199487cd
opeshamz <Samxyn007@yahoo.com>
2023-02-07 02:16:16 +0100

    updates

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
+
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Models\Wallet;
